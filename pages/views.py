c98da445c6023a28a11b12c5467132163e475b7f
Ambrose Adeya Ahawo <ahawoadeya995@gmail.com>
2021-11-23 17:45:27 +0300

    update

--- pages/views.py
+++ pages/views.py
@@ -2,10 +2,13 @@ from django.shortcuts import render
 from django.http import HttpResponse, request
 
 # Create your views here.
-def home_view(*args, **kwargs):
-    # return HttpResponse("<h1> Hello world</h1>") # html string
+def home_view(request, *args, **kwargs):
     return render(request, "home.html", {})
 
 
-def contact_view(*args, **kwargs):
-    return HttpResponse("<h1>Contact page</h1>") # html string
+def about_view(request, *args, **kwargs):
+    return render(request, "about.html", {})
+
+
+def contact_view(request, *args, **kwargs):
+    return render(request, "contact.html", {})
