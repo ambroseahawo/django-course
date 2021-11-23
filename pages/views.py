b6ed387701ff5db0b48108e321df19231638618f
Ambrose Adeya Ahawo <ahawoadeya995@gmail.com>
2021-11-23 19:46:12 +0300

    update

--- pages/views.py
+++ pages/views.py
@@ -7,7 +7,12 @@ def home_view(request, *args, **kwargs):
 
 
 def about_view(request, *args, **kwargs):
-    return render(request, "about.html", {})
+    my_context = {
+        "my_text": "This is about us",
+        "my_number": 148937,
+        "my_list": [834, 19, 28, 'ABC']
+    }
+    return render(request, "about.html", my_context)
 
 
 def contact_view(request, *args, **kwargs):
