308dc6098c9620e4e26a167613baca56c5c1af1a
Ambrose Adeya Ahawo <ahawoadeya995@gmail.com>
2021-11-24 18:11:54 +0300

    update

--- products/forms.py
+++ products/forms.py
@@ -4,11 +4,30 @@ from .models import Product
 
 
 class ProductForm(forms.ModelForm):
+    title = forms.CharField(label='', widget=forms.TextInput(attrs={
+        "placeholder": "Your title"
+    }))  # default required = True
+    description = forms.CharField(required=False, widget=forms.Textarea(attrs={
+        "placeholder": "Your description",
+        "class": "new-class name",
+        "id": "input-id",
+        "rows": 20,
+        "cols":
+        120
+    }))
+    price = forms.DecimalField(initial=199.99)
     class Meta:
         model = Product
-        fields = [
-            'title', 'description', 'price'
-        ]
+        fields = ['title', 'description', 'price']
+    
+    def clean_title(self, *args, **kwargs):
+        title = self.cleaned_data.get("title")
+
+        if "XTER" in title:
+            return title
+        else:
+            raise forms.ValidationError("Your title should contain the chosen word")
+        
 
 
 class RawProductForm(forms.Form):
--- products/views.py
+++ products/views.py
@@ -3,21 +3,21 @@ from .models import Product
 from .forms import ProductForm, RawProductForm
 
 # Create your views here.
-def product_create_view(request):
-    my_form = RawProductForm()
-    if request.method == "POST":
-        my_form = RawProductForm(request.POST)
-        if my_form.is_valid():
-            # now the form is good
-            print(my_form.cleaned_data)
-            Product.objects.create(**my_form.cleaned_data)
-            my_form = RawProductForm()
-        else:
-            print(my_form.errors)
-    context = {
-        "form": my_form
-    }
-    return render(request, 'products/product_create.html', context)
+# def product_create_view(request):
+#     my_form = RawProductForm()
+#     if request.method == "POST":
+#         my_form = RawProductForm(request.POST)
+#         if my_form.is_valid():
+#             # now the form is good
+#             print(my_form.cleaned_data)
+#             Product.objects.create(**my_form.cleaned_data)
+#             my_form = RawProductForm()
+#         else:
+#             print(my_form.errors)
+#     context = {
+#         "form": my_form
+#     }
+#     return render(request, 'products/product_create.html', context)
 
 
 # def product_create_view(request):
@@ -29,15 +29,15 @@ def product_create_view(request):
 #     return render(request, 'products/product_create.html', context)
 
 
-# def product_create_view(request):
-#     form = ProductForm(request.POST or None)
-#     if form.is_valid():
-#         form.save()
-#         form = ProductForm()
+def product_create_view(request):
+    form = ProductForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        form = ProductForm()
 
-#     context ={ 'form': form }
+    context ={ 'form': form }
 
-#     return render(request, "products/product_create.html", context)
+    return render(request, "products/product_create.html", context)
 
 
 def product_detail_view(request):
